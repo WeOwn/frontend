ba62c3aadde7247c71b6f2850c7a5e13f86fc1c8
Anurag7676581 <anurag@heybuddy.co.in>
2024-02-29 15:16:14 +0530

    added api gateway using axios

---
+++ src/service/apiGateway/index.js
@@ -0,0 +1,25 @@
+// apiConfig.js
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.example.com",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
---
+++ src/service/userService/index.js
@@ -0,0 +1,10 @@
+// userService.js
+import api from "../apiGateway";
+
+const userService = {
+  getUser(userId) {},
+  updateUser(userId, data) {},
+  // Add other user-related methods as needed
+};
+
+export default userService;
