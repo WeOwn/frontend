eccacca5a34ae7526bca55c723648d409f64821a
Anurag7676581 <anurag@heybuddy.co.in>
2024-03-05 01:01:33 +0530

    added chips and icons components

--- src/Atoms/Chip/index.jsx
+++ src/Atoms/Chip/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Icon from "../Icon";
+import styles from "./styles.module.css";
+import classNames from "classnames";
 
-const Chip = ({ label = "", icon = "" }) => {
-  return <div>
-    <span>{label}</span>
-    
-  </div>;
+const Chip = ({ label = "Bathroom", icon = "" }) => {
+  return (
+    <div className={classNames(styles.flexRow)}>
+      <Icon name={icon} />
+      <span className={classNames(styles.text)}>{label}</span>
+    </div>
+  );
 };
 
 export default Chip;
