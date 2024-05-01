739336c940136045e67421bbfa2e5d9eba76c58d
Rghv77 <harshraghav718@gmail.com>
2024-05-01 17:34:01 +0530

    Desktop page implemented

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -4,15 +4,17 @@ import Signup from "../pages/signup/Signup";
 import Login from "../pages/login/Login";
 import Home from "../pages/home/Home";
 import OtpModal from "../components/modals/OtpModal";
+import Desktop from "../pages/desktop/Desktop";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path={"/"}>
-        <Route index element={<Home />} />
-        <Route path={"/signup"} element={<Signup />} />
+        <Route index element={<Desktop />} />
+        {/*<Route path={"/signup"} element={<Signup />} />
         <Route path={"/login"} element={<Login />} />
-        <Route path={"/otp"} element={<OtpModal />} />
+        <Route path={"/otp"} element={<OtpModal />} /> */}
+        {/* <Route path={"/desktop"} element={<Desktop />} /> */}
       </Route>
     </Routes>
   );
